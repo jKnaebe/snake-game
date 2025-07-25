08497105e4d4f33120c54755340a64df04e146b9
Jan Knaebe <jm.knaebe@gmail.com>
2025-07-25 14:08:11 +0200

    Added the canvas and the .js File for the snake

---
+++ logic.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById("snakeCanvas");
+let ctx = canvas.getContext("2d");
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 },
+];
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = "lightgreen";
+    ctx.strokeStyle = "darkgreen";
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+drawSnake();
